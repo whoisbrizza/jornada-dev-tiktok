e56bc4a9e62295b5961b7fd6e0294fddb6872f15
whoisbrizza <brizza.adv@hotmail.com>
2023-03-30 22:20:57 -0300

    Update Clone do TikTok - Aula 3

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/Videofooter"
 import "./video.css";
 
 function Video() {
@@ -22,8 +23,9 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
       ></video>
+      <VideoFooter />
     </div>
   );
 }
